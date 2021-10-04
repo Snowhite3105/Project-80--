346b0aaf194fa81fb1f611ed5746f574416bc04f
Snowhite3105 <83868245+Snowhite3105@users.noreply.github.com>
2021-10-04 21:05:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+var GuestName = document.getElementById("name1").value;
+// Use push function//
+document.getElementById("SORT").style.display="block";
+// Display the name in the id=sort_button
+name_of_people
